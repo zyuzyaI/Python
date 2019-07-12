7a90ae38d55c04c110b6ade7d992df0abe5c0fd4
Dima Kynal <dimay90914@gmail.com>
2019-07-12 06:48:30 +0300

    Розклад на просты множники

---
+++ algorythm/Simple Numbers/Розклад на прості множники.py
@@ -0,0 +1,12 @@
+def factor(n):
+    ans = []
+    d = 2
+    while d*d <= n:
+        if n % d == 0:
+            ans.append(d)
+            n //= d
+        else:
+            d += 1
+    if n > 1:
+        ans.append(n)
+    return ans
